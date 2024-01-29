4e4a03a926e3f7026f2240a89af29cbb4f12c1bc
spaceboy000 <cryptospaceboy00@gmail.com>
2024-01-29 21:08:46 +0300

    Update contract

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 export const config = {
     chainId: 5,
     arbAddress: "0xC9247d590376B622B187f1327eBC7885291100a0",
-    contractAddress: "0x527fF30825091031a3E153Ee70AF9Aec6e1a6668", // "0x9D6D69723ED57CdeeF9E8afbE43034e8272172B3",
+    contractAddress: "0x06F0E06214D819DC521cc624e5DFada9d8C56aEF", // "0x9D6D69723ED57CdeeF9E8afbE43034e8272172B3",
     scanLink:
-        "https://arbiscan.io/token/0x527fF30825091031a3E153Ee70AF9Aec6e1a6668",
+        "https://goerli.etherscan.io/address/0x06F0E06214D819DC521cc624e5DFada9d8C56aEF#code",
     telegram_link: "https://t.me/",
     twitter_link: "https://twitter.com/PixelMiner_Arb",
     doc_link: "https://pixel-miner.gitbook.io/pixel-miner/",
